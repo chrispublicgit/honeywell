879eb2e90dddbf65c15605c87dabb4058a3b7f84
Jared Bosco <boscojared@gmail.com>
2020-05-30 00:10:27 -0400

    [meta] set-up logback configuration and create hello world

--- src/main/java/honeywell/Honeywell.java
+++ src/main/java/honeywell/Honeywell.java
@@ -1,7 +1,13 @@
 package honeywell;
 
-public class Honeywell {
-  public static void main(String[] args) {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+public class Honeywell {
+
+  private static final Logger logger = LoggerFactory.getLogger(Honeywell.class);
+
+  public static void main(String[] args) {
+    logger.info("Hello, World!");
   }
 }
